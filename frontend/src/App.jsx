36a9b831fe3420f6c06916f361dd5a636df722e1
Chandan Chaudhary <chandankkumar156@gmail.com>
2025-10-24 11:45:17 +0530

    update in file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
+import Hero from "./Components/Home";
 import About from "./Components/About";
 import Skills from "./Components/Skills";
 import Services from "./Components/Services";
